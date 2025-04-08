ce2a15e12c9c3eb7c3d03fa981f75be6b0061c14
Syrix <125692045+Syrix-Softworks@users.noreply.github.com>
2025-04-08 10:38:39 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#2f3136', color: 'white', height: '100vh' }}>
+      <h1>Full Discord Clone (UI in progress...)</h1>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// Express + Socket.IO server setup
+
+// You would host this with Render or another backend service
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+app.use(cors());
+app.use(express.json());
+
+const users = {};
+const messages = [];
+
+io.on('connection', (socket) => {
+  socket.on('join', (user) => {
+    users[socket.id] = user;
+    io.emit('user-joined', user);
+  });
+
+  socket.on('send-message', (msg) => {
+    messages.push(msg);
+    io.emit('receive-message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    const user = users[socket.id];
+    delete users[socket.id];
+    io.emit('user-left', user);
+  });
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
